4edc348539b8ab5a5d6c7475d2f30def8e484833
Michelle Rivas <00015121@uca.edu.sv>
2024-05-13 23:37:38 -0600

    update: detail view

--- app/src/main/java/com/enriselle/todochan/features/Details.kt
+++ app/src/main/java/com/enriselle/todochan/features/Details.kt
@@ -1,20 +1,93 @@
 package com.enriselle.todochan.features
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.material3.Button
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import com.enriselle.todochan.ui.theme.TodochanTheme
 
 @Composable
 fun Details(
     onBackClick: () -> Unit,
     todo: Todo?
 ) {
-    Column {
-        todo?.let {
-            Text(text = it.title)
-            Text(text = it.description)
-            Text(text = it.dueDate)
+    TodochanTheme {
+        Scaffold(
+            topBar = {
+                Row (
+                    modifier = Modifier.padding(top = 8.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    IconButton(
+                        onClick = { onBackClick() }
+                    ) {
+                        Icon(
+                            imageVector = Icons.Filled.ArrowBack,
+                            contentDescription = "Back",
+                            tint = MaterialTheme.colorScheme.secondary
+                        )
+                    }
+                    if (todo != null) {
+                        Text(
+                            text = todo.title,
+                            style = MaterialTheme.typography.headlineSmall,
+                            color = MaterialTheme.colorScheme.primary
+                        )
+                    }
+                }
+            }
+        ) { paddingContent ->
+            Column(
+                modifier = Modifier
+                    .padding(paddingContent)
+                    .padding(16.dp)
+            ) {
+                todo?.let {
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize(),
+                        verticalArrangement = Arrangement.spacedBy(16.dp)
+                    ) {
+                        Column {
+                            Text(
+                                text = "Descripción",
+                                style = MaterialTheme.typography.labelLarge,
+                                fontWeight = FontWeight.Bold
+                            )
+                            Text(
+                                text = it.description,
+                                style = MaterialTheme.typography.bodyLarge
+                            )
+                        }
+
+                        Column {
+                            Text(
+                                text = "Fecha de entrega",
+                                style = MaterialTheme.typography.labelLarge,
+                                fontWeight = FontWeight.Bold
+                            )
+                            Text(
+                                text = it.dueDate,
+                                style = MaterialTheme.typography.bodyLarge
+                            )
+                        }
+                    }
+                }
+            }
         }
     }
 }
